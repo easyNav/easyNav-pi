2e8c00ae8d0f73282aa01457fcef26cccabd5786
prakashash18 <prakashash18@gmail.com>
2014-10-29 21:42:22 +0800

    Added new startup script for modules, excluding server

---
+++ StartUpPk.py
@@ -0,0 +1,21 @@
+import subprocess
+
+# #startup dispatcher
+dispatcher = subprocess.Popen('easyNav-pi-dispatcher > dispatcher.txt 2>&1', shell=True)
+
+# #startup Nav
+nav = subprocess.Popen('easyNav-pi-nav > navigation.txt 2>&1', shell=True)
+
+voice = subprocess.Popen('sudo python /home/pi/repos/easyNav-IO/voice.py > voice.txt 2>&1', shell=True)
+
+serial = subprocess.Popen('sudo python /home/pi/repos/easyNav-serial/sprotpy/serialmod.py > serial.txt 2>&1' , shell=True)
+
+alert = subprocess.Popen('sudo python /home/pi/repos/easyNav-serial/sprotpy/alert.py > alert.txt 2>&1', shell=True)
+
+cruncher = subprocess.Popen('sudo python /home/pi/repos/easyNav-gear2/Cruncher/cruncher.py pi > cruncher.txt 2>&1', shell=True)
+
+
+
+
+
+
