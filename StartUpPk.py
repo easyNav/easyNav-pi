bac5beaaa80860e6c894571e46bf61a8b1e05f15
prakashash18 <prakashash18@gmail.com>
2014-10-30 15:04:32 +0800

    Added new startup script for modules, excluding server

--- StartUpPk.py
+++ StartUpPk.py
@@ -16,18 +16,42 @@ class StartUp(object):
 		self.speakery=speaker.newSpeaker()
 		self.dispatcher = ""
 		self.nav = ""
+		self.voice = ""
+		self.serial = ""
+		self.alert = ""
+		self.cruncher = ""
 
 	def monitor(self):
 		while(1):
 			if(self.dispatcher.poll() != None): #process died
-				self.speakery.say("Dispatcher Died!!")
+				self.speakery.say("Dispatcher Died!")
 				self.speakery.say("restarting Dispatcher")
 				self.dispatcher = self.startDispatcher()
 
 			if(self.nav.poll() != None): #process died
-				self.speakery.say("Navigation Daemon Died!!")
+				self.speakery.say("Navigation Daemon Died!")
 				self.speakery.say("restarting Navigation")
 				self.nav = self.startNav()
+
+			if(self.voice.poll() != None): #process died
+				self.speakery.say("Voice Daemon Died!")
+				self.speakery.say("restarting Voice")
+				self.voice = self.startVoice()
+
+			if(self.serial.poll() != None): #process died
+				self.speakery.say("Serial Daemon Died!")
+				self.speakery.say("restarting Serial")
+				self.serial = self.startSerial()
+
+			if(self.alert.poll() != None): #process died
+				self.speakery.say("Alert Daemon Died!")
+				self.speakery.say("restarting Alert")
+				self.alert = self.startAlert()
+
+			if(self.cruncher.poll() != None): #process died
+				self.speakery.say("Cruncher Daemon Died!")
+				self.speakery.say("restarting Cruncher")
+				self.cruncher = self.startCruncher()
 
 			time.sleep(3)
 
@@ -42,10 +66,35 @@ class StartUp(object):
 		self.speakery.say("Started Nav")
 		return nav
 
+	def startVoice(self):
+		voice = subprocess.Popen('sudo python /home/pi/repos/easyNav-IO/voice.py > voice.txt 2>&1', shell=True)
+		self.speakery.say("Started Voice")
+		return voice
+
+	def startSerial(self):
+		serial = subprocess.Popen('sudo python /home/pi/repos/easyNav-serial/sprotpy/serialmod.py > serial.txt 2>&1' , shell=True)
+		self.speakery.say("Started Serial")
+		return serial
+
+
+	def startAlert(self):
+		alert = subprocess.Popen('sudo python /home/pi/repos/easyNav-serial/sprotpy/alert.py > alert.txt 2>&1', shell=True)
+		self.speakery.say("Started alert")
+		return alert
+
+	def startCruncher(self):
+		cruncher = subprocess.Popen('sudo python /home/pi/repos/easyNav-gears2/Cruncher/cruncher.py pi > cruncher.txt 2>&1', shell=True)
+		self.speakery.say("Started cruncher")
+		return cruncher
+
 def runMain():
 	startUp = StartUp()
 	startUp.dispatcher = startUp.startDispatcher()
 	startUp.nav = startUp.startNav()
+	startUp.voice = startUp.startVoice()
+	startUp.serial = startUp.startSerial()
+	startUp.alert = startUp.startAlert()
+	startUp.cruncher = startUp.startCruncher()
 	startUp.monitor()
 
 if __name__ == '__main__':
@@ -54,11 +103,8 @@ if __name__ == '__main__':
 
 #monitor forever
 
-#voice = subprocess.Popen('sudo python /home/pi/repos/easyNav-IO/voice.py > voice.txt 2>&1', shell=True)
 
-#serial = subprocess.Popen('sudo python /home/pi/repos/easyNav-serial/sprotpy/serialmod.py > serial.txt 2>&1' , shell=True)
 
-#alert = subprocess.Popen('sudo python /home/pi/repos/easyNav-serial/sprotpy/alert.py > alert.txt 2>&1', shell=True)
 
 #cruncher = subprocess.Popen('sudo python /home/pi/repos/easyNav-gears2/Cruncher/cruncher.py pi > cruncher.txt 2>&1', shell=True)
 
