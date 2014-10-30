1416ec3ab6e762dcd518b9dd7bbe4c9f9daa6e3e
prakashash18 <prakashash18@gmail.com>
2014-10-30 14:45:27 +0800

    Added new startup script for modules, excluding server

--- StartUpPk.py
+++ StartUpPk.py
@@ -1,5 +1,7 @@
 import subprocess
 import time
+import speaker
+
 # #startup dispatcher
 # class StartUp(object):
 # 	def __init__:
@@ -9,22 +11,48 @@ import time
 
 # 		self.nav = 
 
-def monitor(dispatcher):
-	while(1):
-		if(dispatcher.poll() != None): #process died
-			print "Dispatcher Died!!"
+class StartUp(object):
+	def __init__(self):
+		self.speakery=speaker.newSpeaker()
+		self.dispatcher = ""
+		self.nav = ""
 
-		time.sleep(3)
+	def monitor(self):
+		while(1):
+			if(dispatcher.poll() != None): #process died
+				self.speakery.say("Dispatcher Died!!")
+				self.speakery.say("restarting Dispatcher")
+				self.dispatcher = self.startDispatcher()
+
+			if(dispatcher.poll() != None): #process died
+				self.speakery.say("Navigation Daemon Died!!")
+				self.speakery.say("restarting Navigation")
+				self.nav = self.startNav()
+
+			time.sleep(3)
+
+	def startDispatcher(self):
+		dispatcher = subprocess.Popen('easyNav-pi-dispatcher > dispatcher.txt 2>&1', shell=True)
+		self.speakery.say("Started dispatcher")
+		return dispatcher
+
+	def startNav(self):
+		#startup Nav
+		nav = subprocess.Popen('easyNav-pi-nav > navigation.txt 2>&1', shell=True)
+		self.speakery.say("Started Nav")
+		return nav
+
+def runMain():
+	startUp = StartUp()
+	startUp.dispatcher = startUp.startDispatcher()
+	startUp.nav = startUp.startNav()
+	startUp.monitor()
+
+if __name__ == '__main__':
+	runMain()
 
 
-dispatcher = subprocess.Popen('easyNav-pi-dispatcher > dispatcher.txt 2>&1', shell=True)
-print "Started dispatcher"
-
-# #startup Nav
-nav = subprocess.Popen('easyNav-pi-nav > navigation.txt 2>&1', shell=True)
-print "Started Nav"
-
-monitor(dispatcher)
+#monitor forever
 
 #voice = subprocess.Popen('sudo python /home/pi/repos/easyNav-IO/voice.py > voice.txt 2>&1', shell=True)
 
@@ -33,10 +61,4 @@ monitor(dispatcher)
 #alert = subprocess.Popen('sudo python /home/pi/repos/easyNav-serial/sprotpy/alert.py > alert.txt 2>&1', shell=True)
 
 #cruncher = subprocess.Popen('sudo python /home/pi/repos/easyNav-gears2/Cruncher/cruncher.py pi > cruncher.txt 2>&1', shell=True)
-
-
-
-
-
-
 
