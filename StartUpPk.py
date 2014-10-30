50bbdf9bf64964e81e68b8875fb4af06ad115e41
prakashash18 <prakashash18@gmail.com>
2014-10-30 17:38:59 +0800

    test start

--- StartUpPk.py
+++ StartUpPk.py
@@ -60,6 +60,7 @@ class StartUp(object):
 	def startServer(self):
 		serverStarted = False
 		server = subprocess.Popen('cd /home/pi/repos/easyNav-server && node app.js > server.txt 2>&1', shell=True)
+		time.sleep(1)
 		self.speakery.say("Starting server, please wait")
 
 		while(not serverStarted):
