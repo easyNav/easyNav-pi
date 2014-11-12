9dd387e812da23c04796aa6b2b3141d738c2f3e8
prakashash18 <prakashash18@gmail.com>
2014-11-12 17:43:11 +0800

    a

--- reportIP.py
+++ reportIP.py
@@ -3,39 +3,45 @@
 import requests
 import os
 import json
-
-os.system("ifconfig wlan0 | grep inet  > myIp.txt")
-os.system("iwconfig wlan0 | grep ESSID > myESSID.txt")
-
-ipAddr = ""
-givenESSID=""
+import time
 
 
-with open("myIp.txt") as IPText:
-	for line in IPText:
-		if "inet" in line:
-			print line
-			lineDic = line.split(" ")
-			for word in lineDic:
-				print word
-				if "addr" in word:
-					ipAddr = word.replace("addr:", "")
-					print word	
-					
-with open('myESSID.txt') as ESSIDTxt:
-	for line in ESSIDTxt:
-		if "ESSID" in line:
-			lineDic = line.split(" ")
-			for word in lineDic:
-				if "ESSID" in word:
-					givenESSID = word.replace("ESSID:", "")
-					print givenESSID
+while(1):
+
+	os.system("ifconfig wlan0 | grep inet  > myIp.txt")
+	os.system("iwconfig wlan0 | grep ESSID > myESSID.txt")
+
+	ipAddr = ""
+	givenESSID=""
 
 
-#payload={"ip":ipAddr, "essid": givenESSID}
+	with open("myIp.txt") as IPText:
+		for line in IPText:
+			if "inet" in line:
+				print line
+				lineDic = line.split(" ")
+				for word in lineDic:
+					print word
+					if "addr" in word:
+						ipAddr = word.replace("addr:", "")
+						print word	
+						
+	with open('myESSID.txt') as ESSIDTxt:
+		for line in ESSIDTxt:
+			if "ESSID" in line:
+				lineDic = line.split(" ")
+				for word in lineDic:
+					if "ESSID" in word:
+						givenESSID = word.replace("ESSID:", "")
+						print givenESSID
 
-try:
-	r = requests.get("http://54.169.105.67:8002/?ip="+ipAddr+"&essid="+givenESSID)
-	print r
-except: 
-	print "network error, cannot report ip"
+
+	#payload={"ip":ipAddr, "essid": givenESSID}
+
+	try:
+		r = requests.get("http://54.169.105.67:8002/?ip="+ipAddr+"&essid="+givenESSID)
+		print r
+	except: 
+		print "network error, cannot report ip"
+
+	time.sleep(60)
